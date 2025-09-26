9768306b7a69ee6c0b1ffabc1a19b237dfd9bb1b
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:47:54 +0200

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/orders', orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
