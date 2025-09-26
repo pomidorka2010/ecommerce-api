b1ed09fef8c74e636e113498c0c85d6e6874d266
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-26 13:49:21 +0200

    Updated MongoDB connection options for improved compatibility

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
